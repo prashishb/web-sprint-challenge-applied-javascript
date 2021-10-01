84f5db63aadccf93a88bc7e65eed12c346d4dfe6
prashishb <prashb.web@gmail.com>
2021-10-01 08:58:38 -0400

    completed task 1

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,29 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  // creating elements
+  const header = document.createElement('div');
+  const headerDate = document.createElement('span');
+  const headerTitle = document.createElement('h1');
+  const headerTemp = document.createElement('span');
+
+  // assigning classnames to elements
+  header.classList.add('header');
+  headerDate.classList.add('date');
+  headerTemp.classList.add('temp');
+
+  // appending elements
+  header.appendChild('headerDate');
+  header.appendChild('headerTitle');
+  header.appendChild('headerTemp');
+
+  // adding content
+  headerDate.textContent = date;
+  headerTitle.textContent = title;
+  headerTemp.textContent = temp;
+
+  return header;
 }
 
 const headerAppender = (selector) => {
