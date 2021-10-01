127c7db08a7b78e89993b7602e1754133a4d4cb2
prashishb <prashb.web@gmail.com>
2021-10-01 10:13:47 -0400

    completed tabs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -35,6 +35,14 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  axios.get('http://localhost:5000/api/topics')
+    .then(res => {
+      document.querySelector(selector).appendChild(Tabs(res.data.topics));
+    })
+    .catch(err => {
+      console.error(err);
+    })
 }
 
 export { Tabs, tabsAppender }
